6b3de9b7ea2625a28d2e2ef9660dda7354087a24
gsiwatch <gaurishsiwatch@quickenloans.com>
2020-04-22 00:25:44 -0400

    cleaning previous files

--- jsSandbox/js/app.js
+++
@@ -1,8 +0,0 @@
-// following is the console method examples
-
-console.time('match time message'); //
-  console.log('things end here'); // to log message in console
-  console.error('error occured'); // to log the error message in console
-  console.warn('warning message'); // to log the warning message in console
-  // this message has to match the time method in order to display the execution time in console
-console.timeEnd('match time message');
--- misc/jsSandbox/app.js
+++
@@ -1,6 +0,0 @@
-console.time('time');
-  console.log('start writing code');
-  console.warn('this is the warning messsage');
-  console.error('this is error');
-  console.log('things end here');
-console.timeEnd('time');
