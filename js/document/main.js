7ff0d157f8a2a8bf8a0b1cd70b038ddc00dce609
GAURISH SIWATCH <gaurish.siwatch@gmail.com>
2020-05-15 23:05:27 -0400

    adding doc

--- js/document/main.js
+++ js/document/main.js
@@ -2,3 +2,12 @@ const input = window.prompt('Enter your name');
 window.alert('hello ' + input);
 
 // DOM is the structure representation of the html document
+
+/**
+ * document.all:  gives us all the elements as the collection in an array
+ * document.head: will give the head
+ * document.body: will give us the body
+ * document.doctype: will give us the doctype
+ * document.URL: will give us the url
+ * document.contentType: will give us the content Type
+ */
